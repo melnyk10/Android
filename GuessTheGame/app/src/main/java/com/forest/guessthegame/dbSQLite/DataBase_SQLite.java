e24a2b30f5aecd76ac2231a19965381fac5fe5d5
Orest Melnyk <o.melnyk10@gmail.com>
2017-07-19 19:27:53 +0300

    add method getEngName

--- GuessTheGame/app/src/main/java/com/forest/guessthegame/dbSQLite/DataBase_SQLite.java
+++ GuessTheGame/app/src/main/java/com/forest/guessthegame/dbSQLite/DataBase_SQLite.java
@@ -1,6 +1,7 @@
 package com.forest.guessthegame.dbSQLite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,17 +9,16 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by forest on 19.07.2017.
  */
 
-public class DataBase_SQLite extends SQLiteOpenHelper {
+public class DataBase_SQLite extends SQLiteOpenHelper implements ManageDB {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "GuessTheGame";
-    private static final String TABLE_HACKERS_NEWS = "info_about_game";
+    private static final String TABLE_GAMES_INFO = "info_about_game";
     private static final String KEY_ID = "id";
     private static final String KEY_ENG_NAME = "eng_name";
     private static final String KEY_RUS_NAME = "rus_name";
     private static final String KEY_UKR_NAME = "ua_name";
     private static final String KEY_YEAR = "year";
     private static final String KEY_IMG_NAME = "img_name";
-    private static final String KEY_URL = "url";
 
 
     public DataBase_SQLite(Context context) {
@@ -28,11 +28,48 @@ public class DataBase_SQLite extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_GAMES_INFO + " ("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_ENG_NAME + " VARCHAR,"
+                + KEY_RUS_NAME + " VARCHAR" + KEY_UKR_NAME + " VARCHAR" + KEY_YEAR + " VARCHAR"
+                + KEY_IMG_NAME + " VARCHAR" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_GAMES_INFO);
+        onCreate(db);
+    }
 
+    @Override
+    public String getEngName(short id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT "+KEY_ENG_NAME+" FROM "+ TABLE_GAMES_INFO + " WHERE id = "+id, null);
+        int titleIndex = c.getColumnIndex(KEY_ENG_NAME); //індекс колонки "event"
+
+        db.close();
+        c.close();
+
+        return c.getString(titleIndex);
+    }
+
+    @Override
+    public String getRusName(short id) {
+        return null;
+    }
+
+    @Override
+    public String getUaName(short id) {
+        return null;
+    }
+
+    @Override
+    public String getJpgName(short id) {
+        return null;
+    }
+
+    @Override
+    public short getYear(short id) {
+        return 0;
     }
 }
