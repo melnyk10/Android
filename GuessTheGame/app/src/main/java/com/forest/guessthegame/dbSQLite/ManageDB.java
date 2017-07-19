5880c9c33a119ab96ede00473ae255dec1aef229
Orest Melnyk <o.melnyk10@gmail.com>
2017-07-19 19:14:29 +0300

    add ManageDB interface

---
+++ GuessTheGame/app/src/main/java/com/forest/guessthegame/dbSQLite/ManageDB.java
@@ -0,0 +1,13 @@
+package com.forest.guessthegame.dbSQLite;
+
+/**
+ * Created by forest on 19.07.2017.
+ */
+
+public interface ManageDB {
+    String getEngName(short id);
+    String getRusName(short id);
+    String getUaName(short id);
+    String getJpgName(short id);
+    short getYear(short id);
+}
