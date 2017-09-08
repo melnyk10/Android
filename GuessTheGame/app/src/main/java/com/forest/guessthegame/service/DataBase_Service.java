6af3c210c05b895fedc5bcfa8f03323caddd73e0
Orest Melnyk <o.melnyk10@gmail.com>
2017-09-08 19:43:04 +0300

    rename some method

--- GuessTheGame/app/src/main/java/com/forest/guessthegame/service/DataBase_Service.java
+++ GuessTheGame/app/src/main/java/com/forest/guessthegame/service/DataBase_Service.java
@@ -1,20 +1,10 @@
 package com.forest.guessthegame.service;
 
-import java.util.List;
-
-/**
- * Created by forest on 08.09.2017.
- */
 
 public interface DataBase_Service {
-     //String getAnswer();
-     //short randomNameOfGame();
-     void reset();
-     //List<Short> getIdsListOfDB();
-     String getNameOfPic();
-     String getName();
-     String getName(short id);
-     //short getID();
-     short getYear();
-     void setListOfIds(List<Short> listOfIds);
+    void reset();
+    String getPicturesName();
+    String getGamesName();
+    String getGamesNameById(short id);
+    short getYearOfGame();
 }
