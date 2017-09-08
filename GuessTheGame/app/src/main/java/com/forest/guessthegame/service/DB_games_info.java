6abdb4ca33909dbf93b784d1e38a9b492058a6aa
Orest Melnyk <o.melnyk10@gmail.com>
2017-09-08 19:16:26 +0300

    delete all from DB_saveTable

--- GuessTheGame/app/src/main/java/com/forest/guessthegame/service/DB_games_info.java
+++ GuessTheGame/app/src/main/java/com/forest/guessthegame/service/DB_games_info.java
@@ -2,16 +2,15 @@ package com.forest.guessthegame.service;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.Button;
 
-import com.forest.guessthegame.dbSQLite.impl.DB_saveTable;
+import com.forest.guessthegame.dbSQLite.impl.DataBase_SQLite;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DB_games_info {
     private Context context;
-    private DB_saveTable dataBase_sqLite;
+    private DataBase_SQLite dataBase_sqLite;
 
     public List<Short> listOfIds;
     private String answer = "";
@@ -19,7 +18,7 @@ public class DB_games_info {
 
     public DB_games_info(Context context) {
         this.context = context;
-        this.dataBase_sqLite = new DB_saveTable(context);
+        this.dataBase_sqLite = new DataBase_SQLite(context);
         //dataBase_sqLite.onUpgrade(dataBase_sqLite.getReadableDatabase(),1,1);
         this.listOfIds = dataBase_sqLite.getListIds();
     }
@@ -73,44 +72,5 @@ public class DB_games_info {
     public void setListOfIds(List<Short> listOfIds) {
         this.listOfIds = listOfIds;
     }
-
-
-
-
-    public void saveScore(int score){
-        dataBase_sqLite.saveScore(score);
-    }
-
-    public void saveNameOfPic(){
-        dataBase_sqLite.saveNameOfPicFromDB(getNameOfPic());
-    }
-
-    public void saveListOfIDS(String ids){
-        dataBase_sqLite.saveListOfIds(ids);
-    }
-
-    public void saveBtnText(Button btn, String KEY_OF_BTN){
-        dataBase_sqLite.saveBtnText(btn, KEY_OF_BTN);
-    }
-
-    public int getScore(){
-        return dataBase_sqLite.getScore();
-    }
-
-    public String getNameOfPicFromDB(){
-        return dataBase_sqLite.getNameOfPicFromDB();
-    }
-
-    public String getListOfIDS(){
-        return dataBase_sqLite.getListOfIds();
-    }
-
-    public String getBtnText(String KEY_OF_BTN){
-        return dataBase_sqLite.getBtnText(KEY_OF_BTN);
-    }
-
-    public void insert(){dataBase_sqLite.insert();}
-    public void delete(){dataBase_sqLite.delete();}
-    public String printAll(){return dataBase_sqLite.showAll();}
 
 }
